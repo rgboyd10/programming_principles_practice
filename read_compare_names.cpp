d8d1cf5686b1c3cc121bf6cc455037d6ac080a03
rgboyd10 <rgboyd10@gmail.com>
2017-10-22 14:30:14 -0400

    -

---
+++ read_compare_names.cpp
@@ -0,0 +1,21 @@
+//read and compare names
+
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+  cout << "Please enter two names\n";
+  string first;
+  string second;
+
+  cin >> first >> second; //read two strings
+  if(first == second) cout << "That's the same name twice\n";
+  if(first < second)
+    cout << first << " is alphabetically before " << second << '\n';
+  if(first > second)
+    cout << first << " is alphabetically after " << second << '\n';
+}
