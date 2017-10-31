20ebf4d5c7aef40a083a5682d305029ad1c25753
rgboyd10 <rgboyd10@gmail.com>
2017-10-31 19:22:07 -0400

    -

---
+++ ex3-3.cpp
@@ -0,0 +1,13 @@
+/*Write a program that doesn't do anything, but declares a number of variables with legal and illegal names (such as int double = 0;), so that you can see how the compiler reacts.*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int double = 0;
+  double float = 10;
+  fake doule = 100;
+  what is this = 150;
+}
