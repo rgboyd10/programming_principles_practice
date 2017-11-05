3276078fb91fd3333447c775d4cd6751a1d84a25
rgboyd10 <rgboyd10@gmail.com>
2017-11-05 15:52:22 -0500

    -

--- ex3-10.cpp
+++ ex3-10.cpp
@@ -18,12 +18,47 @@ int main()
   string operation;
   
   //request the operator and operands from the user and read them into the variables
-
+  cout << "Please enter the one of the folowed operands:\n +\n -\n *\n /\n plus\n minus\n mul\n div\nfollowed by two numbers of your choice each followed by 'enter':\n";
+  cin >> operation >> operand1 >> operand2;
 
   //calculate the result and store it in a variable
-
+  if(operation == "+")
+    {
+      result = operand1 + operand2;
+    }
+  else if(operation == "-")
+    {
+      result = operand1 - operand2;
+    }
+  else if(operation == "*")
+    {
+      result = operand1 * operand2;
+    }
+  else if(operation == "/")
+    {
+      result = operand1 / operand2;
+    }
+  else if(operation == "plus")
+    {
+      result = operand1 + operand2;
+    }
+  else if(operation == "minus")
+    {
+      result = operand1 - operand2;
+    }
+  else if(operation == "mul")
+    {
+      result = operand1 * operand2;
+    }
+  else if(operation == "div")
+    {
+      result = operand1 / operand2;
+    }
+  else
+    {
+      cout << "You've entered an operation that cannot be completed.";
+    }
 
   //output the result
-
-  
+  cout << "The result of " << operand1 << " " << operation << " " << operand2 << " is " << result << ".\n";  
 }
