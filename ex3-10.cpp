7d07c0804034b6083c9f475a08a47f0002774365
rgboyd10 <rgboyd10@gmail.com>
2017-11-05 14:03:34 -0500

    -

---
+++ ex3-10.cpp
@@ -0,0 +1,29 @@
+/*
+Write a program that takes an operation followed by two operands and outputs the result.  For example:
++100 3.15
+* 4 5
+
+Read the operation into a string called operation and use an if-statement to figure out which operation the user wants, for example if(operation == "+"). Read the operands into variables of type double. implement this for operations called +, -, *, /, plus, minus, mul, and div with their obvious meanings.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  //declare variables
+  double operand1;
+  double operand2;
+  double result;
+  string operation;
+  
+  //request the operator and operands from the user and read them into the variables
+
+
+  //calculate the result and store it in a variable
+
+
+  //output the result
+
+  
+}
