ea8343f135314937229a165b0bc2b8c03d62b9cf
rgboyd10 <rgboyd10@gmail.com>
2017-11-12 16:49:47 -0500

    updating in to cm program

--- in_to_cm.cpp
+++ in_to_cm.cpp
@@ -3,19 +3,22 @@ using namespace std;
 
 //converts from inches to centimeters or centimeters to inches
 //a suffix 'i' or 'c' indicates the unit of the input
+//any other suffix is an error
 
 int main()
 {
   constexpr double cm_per_inch = 2.54; //number of centimeters in an inche
-  double length = 2; //length in inches or cetimeters
+  double length = 1; //length in inches or cetimeters
 
-  char unit = 0;
+  char unit = ''; //a space is not a unit
 
   cout << "Please enter a length followed by a unit (c or i):\n";
   cin >> length >> unit;
 
   if(unit == 'i')
     cout << length << "in ==" << cm_per_inch * length << "cm\n";
-  else
+  else if (unit=='c')
     cout << length << "cm ==" << length / cm_per_inch << "in\n";
+  else
+    cout <<"Sorry, I don't know a unit called '" << unit << "'\n";
 }
