ba97edb7d8aa6956bfc09c4b8a5e834eda7c149b
rgboyd10 <rgboyd10@gmail.com>
2017-11-13 22:25:47 -0500

    -

--- char_values.cpp
+++ char_values.cpp
@@ -5,12 +5,24 @@ using namespace std;
 int main()
 {
   const char ay = 'a';
-  char character_start = 'a';
-  int x;
-  while(x < 26)
+  const char Aay = 'A';
+  char lower_character_start = 'a';
+  char upper_character_start = 'A';
+  
+  for(int x = 0; x < 26; x++)
     {
-      cout << character_start << "\t" <<  ay + x  << "\n";
-      x++;
-      character_start++;
+      cout << lower_character_start << "\t" <<  ay + x  << "\n";
+      lower_character_start++;
+    }
+
+  for(int y = 0; y < 26; y++)
+    {
+      cout << upper_character_start << "\t" << Aay + y << "\n";
+      upper_character_start++;
+    }
+
+  for(int z = 0; z <= 10; z++)
+    {
+      cout << z << "\n";
     }
 }
