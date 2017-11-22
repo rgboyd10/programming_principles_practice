43a4d4bc28230685d7c73a7dc9473568b1ccb6f0
rgboyd10 <rgboyd10@gmail.com>
2017-11-21 22:48:59 -0500

    -

--- terminating_pipe.cpp
+++ terminating_pipe.cpp
@@ -19,10 +19,14 @@ int main()
 	{
 	  cout << "The smaller value is " << userin2 << ".\n The larger value is " << userin1 << ".\n";
 	}
-      else
+      else if(userin2 > userin1)
 	{
 	  cout << "The smaller value is " << userin1 << ".\n The larger value is " << userin2 << ".\n"; 
-	}      
+	}
+      else
+	{
+	  cout << "The values " << userin1 << " and " << userin2 << " are the same.";
+	}
     }
   
   return 0;
