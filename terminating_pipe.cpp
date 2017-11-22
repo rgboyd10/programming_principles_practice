3f7e44e0207ad53775ce145b4f37b4c03bcc611a
rgboyd10 <rgboyd10@gmail.com>
2017-11-21 22:45:49 -0500

    -

--- terminating_pipe.cpp
+++ terminating_pipe.cpp
@@ -7,17 +7,14 @@ int main()
   int userin1;
   int userin2;
   
-  while(cin)
+  while(cin != "|")
     {
       cout << "Please enter two integers, each followed by 'enter'.  If you enter |, the program will end.\n";
-      cin >> userin1 >> userin2;
-
-      if(userin1 == '|' || userin2 == '|')
-	{
-	  cout << "You have entered a pipe.  The program will now end.";
-	  break;
-	}
-
+      
+      cin >> userin1;
+      
+      cin >> userin2;
+      
       if(userin1 > userin2)
 	{
 	  cout << "The smaller value is " << userin2 << ".\n The larger value is " << userin1 << ".\n";
@@ -25,7 +22,8 @@ int main()
       else
 	{
 	  cout << "The smaller value is " << userin1 << ".\n The larger value is " << userin2 << ".\n"; 
-	}
-      
+	}      
     }
+  
+  return 0;
 }
