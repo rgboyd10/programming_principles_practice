56dbb79837526b564ef1b99aba12ea03533e5c47
rgboyd10 <rgboyd10@gmail.com>
2017-12-03 23:30:54 -0500

    -

---
+++ primes.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+  vector<int> primes;
+  //fill the vector with all numbers to be removed upon comparison
+  for(int a = 1; a < 100; a++)
+    primes.push_back(a);
+  
+  //declare for loop to iterate, calculating primes
+  for(int x = 1; x <= 100; x++)
+    {
+      for(int y = 1; y <= 100; y++)
+	{
+	  if(x % y == 0)
+	    {
+	      primes.erase(primes.begin() + x);
+	    }
+	}
+    }
+
+  for(int b = 1; b < primes.size(); b++)
+    {
+      cout << primes[b] << "\n";
+    }
+}
